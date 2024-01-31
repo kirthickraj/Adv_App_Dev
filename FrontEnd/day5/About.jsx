7fd49f12ef11acd3e7630d037222e0b35c8674a4
kirthickraj <119046533+kirthickraj@users.noreply.github.com>
2024-01-31 11:57:38 +0530

    Add files via upload

---
+++ FrontEnd/day5/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../Components/About.css';
+// import About from "./About.jpg" 
+function About() {
+  return (
+    
+    <div className='body'>
+        
+        
+    <div className='flx'><br/><br/><br/><br/>
+      <h1 className='h1'>About Us</h1>
+      <div>
+      <p>
+      Starchess to provide training and mentoring to enthusiastic players all over the world. Provides coaching for various levels based on their skills and need, either personal coaching or Group camps .Coaching is being Important at our academy or online.
+      </p>
+      <p>
+      Age of Entry :The entry age of online training is 6 years. The entry age of onboard – training is 5.5 Years
+      </p>
+      <p>
+      Our Recommendation : We recommend weekly 2 classes / Monthly 8 classes.
+Training Method : Training through starchess website
+Students : Starchess having students across UAE, US, UK etc.,
+
+
+      </p>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
