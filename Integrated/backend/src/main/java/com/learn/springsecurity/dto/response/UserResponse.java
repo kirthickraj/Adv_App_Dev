a8aec10d89e4577c58fe3d54013ea4250d98e57d
kirthickraj <119046533+kirthickraj@users.noreply.github.com>
2024-02-15 12:11:59 +0530

    Add files via upload

---
+++ Integrated/backend/src/main/java/com/learn/springsecurity/dto/response/UserResponse.java
@@ -0,0 +1,19 @@
+package com.learn.springsecurity.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String ConfirmPassword;
+}
